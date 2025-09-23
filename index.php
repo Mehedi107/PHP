0e15a34908ef3ae6aa0d39b0233fcece9e003af3
Mehedi <mehedi.hsn.dev@gmail.com>
2025-09-23 15:05:17 +0600

    hello php

---
+++ index.php
@@ -0,0 +1,9 @@
+<!-- ++++++++++++++++++++++ -->
+<!-- Class 1 -->
+<!-- ++++++++++++++++++++++ -->
+
+<!-- <?php if (true) { ?>
+    <?php // this is how we write comment in php! 
+    ?>
+    <p>Some HTML text!</p>
+<?php } ?> -->
