3f9282fcec7bb2c4484ef49585ea4518130215f1
Mehedi <mehedi.hsn.dev@gmail.com>
2025-09-23 15:08:42 +0600

    Pre-defined variable in php

--- index.php
+++ index.php
@@ -41,3 +41,46 @@
     ?>
 </body>
 </html> -->
+
+<!-- +++++++++++++++++++++++++++++++++++++ -->
+<!-- Class 3 (Pre-defined variable in php) -->
+<!-- +++++++++++++++++++++++++++++++++++++ -->
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pre-defined variable in php</title>
+</head>
+
+<body>
+    <h1>Pre-defined variable in php</h1>
+    <?php
+    echo $_SERVER['DOCUMENT_ROOT'];
+    echo "<br>";
+    echo $_SERVER['PHP_SELF'];
+    echo "<br>";
+    echo $_SERVER['SERVER_NAME'];
+    echo "<br>";
+    echo $_SERVER['REQUEST_METHOD'];
+
+    echo "<br>";
+    echo $_GET["name"];
+    echo "<br>";
+    echo $_REQUEST["name"]; // not recomended
+    echo "<br>";
+    // echo $_FILES["name"];
+    echo "<br>";
+    // echo $_COOKIE["name"];
+    echo "<br>";
+    $_SESSION["myName"] = "Mehedi hasan";
+    echo $_SESSION["myName"];
+    echo "<br>";
+    // echo $_ENV[""];
+
+    ?>
+</body>
+
+</html> -->
