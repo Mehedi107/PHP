aeca8107ac757361dbaed1aeadc83d15e82688ec
Mehedi <mehedi.hsn.dev@gmail.com>
2025-09-23 15:06:15 +0600

    Data type in PHP

--- index.php
+++ index.php
@@ -7,3 +7,37 @@
     ?>
     <p>Some HTML text!</p>
 <?php } ?> -->
+
+
+<!-- ++++++++++++++++++++++ -->
+<!-- Class 2 (Data Types) -->
+<!-- ++++++++++++++++++++++ -->
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <p>This is a paragraph text!</p>
+
+    <?php echo 'This is also a paragraph text!' ?>
+
+    <?php
+    // scalar types (single value)
+    $string = "Mehedi";
+    $int = 123;
+    $float = 20.36;
+    $bool = true;
+
+    // array type
+    $arr1 = array('john', 'doe', 'smith');
+    $arr2 = ['john', 'doe', 'smith'];
+
+    // object types
+    // $obj = new Car();
+    ?>
+</body>
+</html> -->
